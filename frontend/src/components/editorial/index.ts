bdc643924ec045dab6965f0c1a2281ca05b45c19
Alune <alune0225@gmail.com>
2026-06-13 11:51:40 +0800

    feat(editorial): 暴露 5 个学术感组件 barrel 导出

---
+++ frontend/src/components/editorial/index.ts
@@ -0,0 +1,14 @@
+export { ChapterNumber } from "./ChapterNumber";
+export type { ChapterNumberProps } from "./ChapterNumber";
+
+export { NamePlate } from "./NamePlate";
+export type { NamePlateCandidate, NamePlateProps } from "./NamePlate";
+
+export { Wordmark } from "./Wordmark";
+export type { WordmarkProps, WordmarkSize, WordmarkVariant } from "./Wordmark";
+
+export { StatusPill } from "./StatusPill";
+export type { StatusPillProps, StatusPillVariant } from "./StatusPill";
+
+export { EmptyState } from "./EmptyState";
+export type { EmptyStateAction, EmptyStateProps, EmptyStateTone } from "./EmptyState";
